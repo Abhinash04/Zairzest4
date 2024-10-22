5cab91a6fe4199ebd085df50ad5c34cc052d1a9b
Abhinash04 <abhinash.pritiraj@gmail.com>
2024-10-22 23:14:01 +0530

    Profile done

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import './Profile.scss';
+import axios from 'axios'
+import Footer from '../../component/Footer/Footer'
+import Navbar from '../../component/Navbar/Navbar'
+import './Profile.scss'
 
 const LazyFooter = React.lazy(() => import('../../component/Footer/Footer'));
 const LazyNavbar = React.lazy(() => import('../../component/Navbar/Navbar'));
 
-const Profile = ({ username = "Abhinash Pritiraj", zen_id = "2211100553" }) => {
+const Profile = ({username="Abhinash Pritiraj",zen_id="2211100553"}) => {
   const [data, setData] = useState();
   const [userDetails, setUserDetails] = useState();
   const [profileImage, setProfileImage] = useState(null);
 
   const fetchData = async () => {
-    const authToken = sessionStorage.getItem('Auth Token');
+    const authToken = sessionStorage.getItem('Auth Token')
     try {
       const response = await Promise.all([
         axios.get(process.env.REACT_APP_API_LUSER, {
@@ -20,48 +22,38 @@ const Profile = ({ username = "Abhinash Pritiraj", zen_id = "2211100553" }) => {
             'Authorization': `Bearer ${authToken}`
           }
         })
-      ]);
-
+      ])
       const res = response?.map((result, index) => {
-        if (index === 0) return result.data.events;
-        else return result.data;
-      });
-      setData(res[0]);
-      setUserDetails(res[1]);
+        if(index === 0)
+        return result.data.events
+        else
+        return result.data
+      })
+      setData(res[0])
+      setUserDetails(res[1])
     } catch (error) {
       console.log(error);
     }
-  };
+  }
 
   useEffect(() => {
     fetchData();
     const loadImage = async () => {
-      try {
-        const randomImage = await import(`../../assets/images/avatar${zen_id % 10}.jpg`);
-        setProfileImage(randomImage.default);
-      } catch (err) {
-        console.log("Error loading profile image: ", err);
-      }
+      const randomImage = await import(`../../assets/images/avatar${zen_id%10}.jpg`);
+      setProfileImage(randomImage.default);
     };
     loadImage();
-  }, [zen_id]);
+  }, []);
 
   return (
     <div>
-      <React.Suspense fallback={<div>Loading Navbar...</div>}>
+      <React.Suspense fallback={<div>Loading...</div>}>
         <LazyNavbar />
       </React.Suspense>
       <div className="profile_container">
         <div className="profile-box">
           {profileImage && (
-            <div
-              className="profilesubitem1"
-              style={{ 
-                backgroundImage: `url(${profileImage})`, 
-                backgroundSize: 'cover', 
-                backgroundPosition: 'center' 
-              }}
-            ></div>
+            <div className="profilesubitem1" style={{ backgroundImage: `url(${profileImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
           )}
           <div className="profilesubitem2">
             <h1>{username}</h1>
@@ -69,7 +61,7 @@ const Profile = ({ username = "Abhinash Pritiraj", zen_id = "2211100553" }) => {
           </div>
         </div>
       </div>
-      <React.Suspense fallback={<div>Loading Footer...</div>}>
+      <React.Suspense fallback={<div>Loading...</div>}>
         <LazyFooter />
       </React.Suspense>
     </div>
